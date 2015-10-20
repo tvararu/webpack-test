04f6fba97bd679ffcdad557c4c3c1bfa2220d2f8
Theodor Vararu <theo@vararu.org>
2015-10-20 21:34:53 +0100

    Add dlls.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
-module.exports = {
-  devtool: null,
-  debug: false,
-  entry: {
-    app: './src/A.js'
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = [
+  {
+    entry: {
+      A: './src/A.js'
+    },
+    output: {
+      filename: '[name].js'
+    },
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: '.',
+        manifest: require('./B-manifest.json')
+      }),
+      new webpack.DllReferencePlugin({
+        context: '.',
+        manifest: require('./C-manifest.json')
+      })
+    ]
   },
-  output: {
-    filename: '[name].js'
+  {
+    entry: {
+      B: ['./src/B.js']
+    },
+    output: {
+      filename: '[name].js'
+    },
+    plugins: [
+      new webpack.DllPlugin({
+        path: path.resolve(__dirname, 'B-manifest.json'),
+        name: '[name]_[hash]'
+      })
+    ]
+  },
+  {
+    entry: {
+      C: ['./src/C.js']
+    },
+    output: {
+      filename: '[name].js'
+    },
+    plugins: [
+      new webpack.DllPlugin({
+        path: path.resolve(__dirname, 'C-manifest.json'),
+        name: '[name]_[hash]'
+      })
+    ]
   }
-}
+]
